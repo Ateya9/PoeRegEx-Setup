45f61314376aa4a25849370e399f0c2f3549839c
Fish <Mark.fish272@gmail.com>
2022-04-12 01:13:41 +0800

    Updated remove_preceding_mod_info to account for monster cast speed and less curse affect. List of mods is ready to be written to file

--- parse_list.py
+++ parse_list.py
@@ -1,7 +1,6 @@
 import os
 import re
 
-
 output_path = os.fspath("./output/")
 input_file_path = os.fspath("./input.txt")
 log_file_path = os.fspath(output_path + "/log.txt")
@@ -31,22 +30,37 @@ def parse_list() -> None:
             elif "increased Rarity of Items found in this Area" in line:
                 continue
             elif line.startswith(("map", "Prefix", "Suffix")):
-                # Ignore irrelevant lines like 'map extra content weighting [0]'
+                # Skip irrelevant lines like 'map extra content weighting [0]'
                 continue
             elif "increased Pack size" in line:
                 # TODO: code to insert the data into the modgroup file
+                print(mod_group)
                 mod_group = []
                 continue
             output_line = line
             output_line = remove_preceding_mod_info(output_line)
             if output_line.startswith("Total"):
-                # There should be one line with just 'Total', ignore this line.
+                # There should be one line with just 'Total', skip this line.
                 continue
-            print(output_line)
+            output_line = replace_numbers_and_ranges(output_line)
+            output_line = output_line.strip()
+            mod_group.append(output_line)
 
 
 def generate_mod_group_name(mod_group: list) -> str:
     pass
+
+
+def replace_numbers_and_ranges(string_to_process: str) -> str:
+    """
+    Replaces all mod ranges (eg (10-20)%) and numbers with a hash.
+    :param string_to_process:
+    :return:
+    """
+    output = string_to_process
+    output = re.sub(r'\(\d+–\d+\)', "#", output)
+    output = re.sub(r'\d+', "#", output)
+    return output
 
 
 def remove_preceding_mod_info(string_to_process: str) -> str:
@@ -77,17 +91,25 @@ def remove_preceding_mod_info(string_to_process: str) -> str:
                  "critical",
                  "defences",
                  "resistance"]
+    if "increased Monster Cast Speed" in output:
+        # This line actually has relevant numbers here, so we don't want to continue removing numbers.
+        return output
     while re.match("^[0-9].*$", output):
         # Remove all numbers at the start of the line.
         output = output[1:]
     i = 0
     while i < len(mod_types):
+        # Then remove all 'mod types' from the start of the line.
         if output.startswith(mod_types[i]):
             output = output.replace(mod_types[i], "", 1)
-            # print(output)
             i = 0
         else:
             i += 1
+    if "less effect of Curses on Monsters" in output:
+        # This line has relevant numbers after two numbers that need to be removed.
+        output = output[2:]
+        return output
+
     while re.match("^[0-9].*$", output):
         # Remove numbers that appear just after the mod types.
         output = output[1:]
