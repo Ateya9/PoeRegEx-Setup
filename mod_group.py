fe9d9bbf2f96a85a424bc6091d789ab7273bfa86
Fish <Mark.fish272@gmail.com>
2022-04-13 01:04:17 +0800

    Created and fleshed out a ModGroup class

---
+++ mod_group.py
@@ -0,0 +1,22 @@
+import os
+import re
+
+
+class ModGroup:
+    def __init__(self, mod_group_name: str) -> None:
+        super().__init__()
+        self.__mod_group_name = mod_group_name
+        self.__mod_group_file_location = "./output/" + self.__mod_group_name
+        if not os.path.exists(self.__mod_group_file_location):
+            raise FileExistsError(mod_group_name + " doesn't exist.")
+        self.__mods = []
+        with open(self.__mod_group_file_location, "r") as mod_file:
+            for line in mod_file:
+                if line.strip():
+                    self.__mods.append(line.strip())
+
+    def check_for_regex_match(self, regex: str) -> bool:
+        for mod in self.__mods:
+            if re.search(regex, mod):
+                return True
+        return False
