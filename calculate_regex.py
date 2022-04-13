80e9c079e87bf9ecc03c79992a097be236977b45
Fish <Mark.fish272@gmail.com>
2022-04-13 16:45:07 +0800

    Added handling if a mod has a '+' in it.

--- calculate_regex.py
+++ calculate_regex.py
@@ -38,9 +38,6 @@ def calculate_regex_matches() -> None:
             for test_mod_group in mod_groups:
                 if test_mod_group.check_for_regex_match(potential_regex):
                     number_of_matches += 1
-                    print(potential_regex + " is a match for " + test_mod_group.mod_group_name)
-                else:
-                    print(potential_regex + " is NOT a match for " + test_mod_group.mod_group_name)
 
 
 def generate_potential_regexes(mod_group: ModGroup) -> list[str]:
@@ -52,16 +49,16 @@ def generate_potential_regexes(mod_group: ModGroup) -> list[str]:
             # Take snips of varying lengths (2 to 4)
             for i in range(len(mod) - snip_length + 1):
                 potential_regex = mod[i:i + snip_length]
-                potential_regex = potential_regex.replace("#", "\\d*")
+                potential_regex = replace_invalid_regex_chars(potential_regex)
                 output.append(potential_regex)
         # Take snips with space between them (varying space between but snips are always 2 long)
         for i in range(len(mod) - 1):
             space_between_snips = 0
             for y in range(i + 2, len(mod) - 1):
                 first_snip = mod[i:i + 2]
-                first_snip = first_snip.replace("#", "\\d*")
+                first_snip = replace_invalid_regex_chars(first_snip)
                 second_snip = mod[y:y + 2]
-                second_snip = second_snip.replace("#", "\\d*")
+                second_snip = replace_invalid_regex_chars(second_snip)
                 match space_between_snips:
                     case 0:
                         inbetween_snip = ""
@@ -72,6 +69,12 @@ def generate_potential_regexes(mod_group: ModGroup) -> list[str]:
                 final_regex = first_snip + inbetween_snip + second_snip
                 output.append(final_regex)
                 space_between_snips += 1
+    return output
+
+
+def replace_invalid_regex_chars(input_regex: str) -> str:
+    output = input_regex.replace("#", "\\d*")
+    output = output.replace("+", r"\+")
     return output
 
 
