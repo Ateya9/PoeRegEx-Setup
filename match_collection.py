c0c2e1356696d6aa7736b1d364e7f98c7172fc10
Fish <Mark.fish272@gmail.com>
2022-04-13 17:24:54 +0800

    Created MatchCollection class

---
+++ match_collection.py
@@ -0,0 +1,26 @@
+class MatchCollection:
+
+    max_match_dict_size = 3
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.one_way_matches = dict()
+        self.two_way_matches = dict()
+        self.three_way_matches = dict()
+
+    def __add_to_dict(self, key: set, value: str) -> None:
+        match len(key):
+            case 1:
+                dict_to_add_to = self.one_way_matches
+            case 2:
+                dict_to_add_to = self.two_way_matches
+            case 3:
+                dict_to_add_to = self.three_way_matches
+            case _:
+                return
+        if key in dict_to_add_to:
+            if len(dict_to_add_to[key]) < len(value):
+                # If the regex that's already in as a match is shorter than what is trying
+                # to be added, don't overwrite it.
+                return
+        dict_to_add_to[key] = value
